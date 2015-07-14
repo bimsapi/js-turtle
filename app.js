84d708142311f99422101a514f8085bc5d1bf1be
balcorn <ralcorn@eab.com>
2015-07-13 22:31:38 -0400

    docs; code consolidation and error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function () {
+    //core methods
     var loadSample = function(sample) {
             $.ajax({
             url: "./examples/" + sample,
@@ -9,14 +10,31 @@
         });
     }
     
+    var runCode = function() {
+        try {
+            var code = window.editor.getValue();
+            eval(code);
+        }
+        catch(e) {
+            $("#alertBox button").after('<span>There was an error with your code. Check the developer console.</span>');
+            $("#alertBox").fadeIn("slow");
+            console.log(e);
+        }
+    }
+    
+    //event handlers and UI hooks.
+    $('#alertBox .close').click(function(e) {
+        $("#alertBox span").remove();
+        $("#alertBox").hide();
+    });    
+    
     $('#samples').change(function() {
         loadSample($(this).val());
         window.editor.focus();
     });
     
     $('#runButton').click(function() {
-        var code = window.editor.getValue();
-        eval(code);
+        runCode();
         window.editor.focus();
     })
     
@@ -30,23 +48,19 @@
         window.editor.focus();
     });
     
+    //editor setup
     $(document).ready(function() {
         window.editor = ace.edit("editor");
         window.editor.setTheme("ace/theme/monokai");
+        window.editor.$blockScrolling = Infinity;
         window.editor.getSession().setMode("ace/mode/javascript");
+        
+        //bind a key to auto-run the code
         window.editor.commands.addCommand({
             name: 'execScript',
             bindKey: {win: 'Ctrl-M',  mac: 'Command-M'},
             exec: function(editor) {
-                var code = window.editor.getValue();
-                try {
-                  // execute any code in the definitions box
-                  eval(code);
-                } catch(e) {
-                  alert('Exception thrown, please see console');
-                  throw e;
-                } finally {
-                }
+                runCode();
             },
             readOnly: true // false if this command should not apply in readOnly mode
         });
